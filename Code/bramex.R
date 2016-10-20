836d8e8a7160356c0e2ffdcff958055e90c63441
cinnData <canela.miguel@icloud.com>
2016-10-20 13:46:38 +0200

    Add files via upload

---
+++ Code/bramex.R
@@ -0,0 +1,33 @@
+## bramex data set ##
+
+# Setting working directory (Customize)
+setwd("/Users/miguel/Dropbox/Current jobs/STAT-2016")
+
+# Importing data #
+bramex = read.csv("Data/bramex.csv")
+str(bramex)
+summary(bramex)
+
+# Regression line #
+reg.model = lm(formula=mex ~ bra, data=bramex)
+names(reg.model)
+reg.model$coefficients
+summary(reg.model)
+summary(reg.model)$r.squared
+
+# Correlation analysis #
+cor(bramex$bra, bramex$mex)
+cor(bramex[, -1])
+
+# Figure 1.1 #
+rm(list=ls(all=T))
+pdf("Figures/fig 1.1.pdf",width=3.25,height=3.5,pointsize=7)
+plot(bramex$bra, bramex$mex, main="", xlab="Brazil", ylab="Mexico", pch=20)
+dev.off()
+
+# Figure 1.2 #
+rm(list=ls(all=T))
+pdf("Figures/fig 1.2.pdf",width=3.25,height=3.5,pointsize=7)
+plot(bramex$bra, bramex$mex, main="", xlab="Brazil", ylab="Mexico", pch=20)
+abline(model)
+dev.off()
