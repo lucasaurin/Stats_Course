af3569e762d067999d38ca0e681e19c8beaaaa76
cinnData <canela.miguel@icloud.com>
2017-11-15 12:55:19 +0100

    Update lwages.R

--- Code/lwages.R
+++ Code/lwages.R
@@ -1,18 +1,15 @@
 ## lwages data set ##
 
-# Importing data #
-setwd("/Users/miguel/Dropbox/Current jobs/STAT-2016")
-df = read.csv("Data/lwages.csv")
-str(df)
-N = nrow(df)
+# Importing data (customize) #
+setwd("/Users/miguel/Dropbox/STAT-2017")
+lwage <- read.csv("Data/lwages.csv")
+str(lwage)
+N <- nrow(lwage)
 
 # Salary change #
-change <- df$lwage7 - df$lwage0
-change_alt <- exp(df$lwage7) - exp(df$lwage0)
+change <- lwage$lwage7 - lwage$lwage0
 
-# Histograms #
-par(mfrow=c(1,2))
-hist(change_alt, main="", xlab="Wages")
+# Histogram #
 hist(change, main="", xlab="Log wages")
 
 # Confidence limits #
@@ -21,15 +18,14 @@ ucl <- mean(change) + qt(0.975, df=N-1)*sd(change)/sqrt(N)
 
 # t statistic #
 t <- mean(change)/(sd(change)/sqrt(N))
-t > qt(0.975, df=N-1)
+t_alpha <- qt(0.975, df=N-1)
 
 # p-value #
 p <- 2*(1 - pt(t, df=N-1))
 
 # Function t.test #
 t.test(change)
-t.test(df$lwage7, df$lwage0, paired=T)
+t.test(lwage$lwage7, lwage$lwage0, paired=T)
 
 # Correlation #
-plot(lwage7 ~ lwage0, data=df, xlab="Log wages (1980)", ylab="Log wages (1987)", pch=20)
-
+plot(lwage7 ~ lwage0, data=lwage, xlab="Log wages (1980)", ylab="Log wages (1987)", pch=20)
