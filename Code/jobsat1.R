289e6b4f7735c7644b1c1bec5b37036ef7970f0b
cinnData <canela.miguel@icloud.com>
2017-11-15 09:13:46 +0100

    Update jobsat1.R

--- Code/jobsat1.R
+++ Code/jobsat1.R
@@ -1,15 +1,15 @@
 ## jobsat1 data set ##
 
 # Importing data #
-setwd("/Users/miguel/Dropbox/Current jobs/STAT-2016")
-df = read.csv("Data/jobsat1.csv")
-str(df)
+setwd("/Users/miguel/Dropbox/STAT-2017")
+jobsat <- read.csv("Data/jobsat1.csv")
+str(jobsat)
 
 # Formula #
 fm = sat ~ country
 
 # t test (equal variances) #
-df1 = df[df$country!="SP", ]
+df1 <- jobsat[jobsat$country!="SP", ]
 t.test(formula=fm, data=df1, var.equal=T)
 
 # t test (alternative) #
@@ -17,23 +17,8 @@ t.test(formula=fm, data=df1, var.equal=F)
 
 # Group distributions #
 par(mfrow=c(1,2))
-hist(df$sat[df$country=="CH"], main="", xlab="Job satisfaction (Chile)")
-hist(df$sat[df$country=="ME"], main="", xlab="Job satisfaction (Mexico)")
+hist(jobsat$sat[jobsat$country=="CH"], main="",
+ xlab="Job satisfaction (Chile)")
+hist(jobsat$sat[jobsat$country=="ME"],
+  main="", xlab="Job satisfaction (Mexico)")
 
-# One-way ANOVA test #
-aov.model = aov(formula=fm, data=df)
-summary(aov.model)
-
-# Residual analysis #
-hist(aov.model$residuals, main="", xlab="ANOVA residuals")
-qqnorm(aov.model$residuals, main="", xlab="Standard normal quantiles", ylab="ANOVA residuals",
-  pch=20)
-qqline(aov.model$residuals)
-
-# Linear regression (2 groups) #
-reg.model1 = lm(formula=fm, data=df1)
-summary(reg.model1)
-
-# Linear regression (3 groups) #
-reg.model2 = lm(formula=fm, data=df)
-summary(reg.model2)
