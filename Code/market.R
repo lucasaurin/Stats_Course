402d42e6293ccceb95bd178648e4200436bdc395
cinnData <canela.miguel@icloud.com>
2017-11-19 20:30:19 +0100

    Update market.R

--- Code/market.R
+++ Code/market.R
@@ -1,28 +1,29 @@
 ## market data set ##
 
-# Importing data
-setwd("/Users/miguel/Dropbox/Current jobs/STAT-2016")
-df = read.csv("Data/market.csv")
-N = nrow(df)
+# Importing data (custopmize) #
+market <- read.csv("market.csv")
+N <- nrow(market)
 
-# Regression model
-mod = lm(formula=salary ~ market, data=df)
+# Regression model #
+fm <- salary ~ market
+mod <- lm(formula=fm, data=market)
 summary(mod)
 
-# Graphics (1)
-plot(df$market, df$salary, pch=".", xlab="Marketability", ylab="Salary")
+# Graphics (1) #
+plot(formula=fm, data=market, pch=".", xlab="Marketability", ylab="Salary")
 abline(coefficients(mod))
 
-# Graphics (2)
-res = mod$residuals
-lab1 = c("Standard normal quantiles"); lab2 = c("Residuals")
+# Graphics (2) #
+par(mfrow=c(1,1))
+res <- mod$residuals
+lab1 <- c("Standard normal quantiles"); lab2 <- c("Residuals")
 hist(res, main="", xlab=lab2)
 qqnorm(res, main="", xlab=lab1, ylab=lab2)
 qqline(res)
 
-# Normality check
+# Normality check #
 library(e1071)
 skewness(res)
 kurtosis(res)
-JB = (N/6)*(skewness(res)^2 + (kurtosis(res)^2/4))
-P = 1-pchisq(JB, df=2)
+JB <- (N/6)*(skewness(res)^2 + (kurtosis(res)^2/4))
+P <- 1 - pchisq(JB, df=2)
