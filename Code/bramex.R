6498eb7acdd5c2c02d42d67fcf71f28dfb206898
mcanela-iese <38321496+mcanela-iese@users.noreply.github.com>
2018-10-24 11:06:04 +0200

    Update bramex.R

--- Code/bramex.R
+++ Code/bramex.R
@@ -1,31 +1,31 @@
 ## bramex data set ##
 
-# Setting working directory (customize)
-setwd("C:/Users/mcanela/Dropbox (Personal)/STAT-2017")
-
 # Importing data #
-bramex <- read.csv("Data/bramex.csv")
-str(bramex)
-summary(bramex)
+df = read.csv('https://raw.githubusercontent.com/mcanela-iese/Stats_Course/master/Data/bramex.csv')
+str(df)
+head(df)
+tail(df)
+summary(df)
 
 # Regression line #
-reg.model <- lm(formula=mex ~ bra, data=bramex)
-names(reg.model)
-reg.model$coefficients
-summary(reg.model)
-summary(reg.model)$r.squared
+regmod = lm(formula=mex ~ bra, data=df)
+names(regmod)
+regmod$coefficients
+summary(regmod)
+summary(regmod)$r.squared
 
 # Correlation analysis #
-cor(bramex$bra, bramex$mex)
-cor(bramex[, -1])
+cor(df$bra, df$mex)
+cor(df[, -1])
+cov(df[, -1])
 
-# Figure 2.1 (customize) #
-pdf("Figures/fig 1.1.pdf", width=3.25, height=3.5, pointsize=7)
-plot(bramex$bra, bramex$mex, main="", xlab="Brazil", ylab="Mexico", pch=20)
+# Figure 1 (customize) #
+pdf('Figures/fig 1.1.pdf', width=3.25, height=3.5, pointsize=7)
+plot(bramex$bra, bramex$mex, main='', xlab='Brazil', ylab='Mexico', pch=20)
 dev.off()
 
-# Figure 2.2 (customize) #
-pdf("Figures/fig 1.2.pdf", width=3.25, height=3.5, pointsize=7)
-plot(bramex$bra, bramex$mex, main="", xlab="Brazil", ylab="Mexico", pch=20)
-abline(reg.model)
+# Figure 2 (customize) #
+pdf('Figures/fig 1.2.pdf', width=3.25, height=3.5, pointsize=7)
+plot(df$bra, df$mex, main='', xlab='Brazil', ylab='Mexico', pch=20)
+abline(regmod)
 dev.off()
