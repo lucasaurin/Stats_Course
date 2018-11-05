ca0b7a95b78ee6b88040a92b16122acc79d705d3
mcanela-iese <38321496+mcanela-iese@users.noreply.github.com>
2018-11-05 14:16:22 +0100

    Update strike.R

--- Code/strike.R
+++ Code/strike.R
@@ -1,29 +1,26 @@
 ## strike data set ##
 
-# Setting working directory (customize)
-setwd("/Users/miguel/Dropbox/STAT-2017")
-
 # Importing data #
-strike <- read.csv("Data/strike.csv")
-str(strike)
-summary(strike)
+df = read.csv('https://raw.githubusercontent.com/mcanela-iese/Stats_Course/master/Data/strike.csv')
+str(df)
+summary(df)
 
-# Figure 8.1 (customize) #
-pdf("Figures/fig 8.1.pdf", width=3.25, height=3.5, pointsize=7)
-hist(strike$duration, main="", xlab="Strike duration (days)")
+# Figure 4.1 (customize) #
+pdf("Figures/fig 4.1.pdf", width=3.25, height=3.5, pointsize=7)
+hist(df$duration, main="", xlab="Strike duration (days)")
 dev.off()
 
 # Mean and standard deviation #
-mean(strike$duration)
-sd(strike$duration)
+mean(df$duration)
+sd(df$duration)
 
 # Skewness and kurtosis (install e1071 package first) #
 library(e1071)
-skewness(strike$duration)
-kurtosis(strike$duration)
+skewness(df$duration)
+kurtosis(df$duration)
 
 # Compare with random sample from exponential #
-x <- rexp(1000, rate=1/42.67)
+x = rexp(1000, rate=1/42.67)
 mean(x)
 sd(x)
 skewness(x)
