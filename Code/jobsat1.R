252d2d5c2f5336984e95e2e09495200ffcd1b9e1
mcanela-iese <38321496+mcanela-iese@users.noreply.github.com>
2018-11-15 16:36:08 +0100

    Update jobsat1.R

--- Code/jobsat1.R
+++ Code/jobsat1.R
@@ -1,48 +1,44 @@
 ## jobsat1 data set ##
 
-# Importing data (customize) #
-jobsat <- read.csv("jobsat1.csv")
-str(jobsat)
+# Importing data #
+df = read.csv('https://raw.githubusercontent.com/mcanela-iese/Stats_Course/master/Data/jobsat1.csv')
+str(df)
 
 # Formula #
-fm <- sat ~ country
+fm = sat ~ country
 
 # t test (equal variances) #
-t.test(formula=fm, data=jobsat[jobsat$country!="SP", ], var.equal=T)
-t.test(jobsat$sat[jobsat$country=="CH"], jobsat$sat[jobsat$country=="ME"],
-  paired=F)
+t.test(formula=fm, data=df[df$country!='SP', ], var.equal=TRUE)
 
-# t test (alternative) #
-t.test(formula=fm, data=df1, var.equal=F)
+# t test (unequal variances) #
+t.test(formula=fm, data=df[df$country!='SP', ], var.equal=FALSE)
+t.test(df$sat[df$country=='CH'], df$sat[df$country=='ME'], paired=FALSE)
 
 # Group distributions #
 par(mfrow=c(1,2))
-hist(jobsat$sat[jobsat$country=="CH"], main="",
- xlab="Job satisfaction (Chile)")
-hist(jobsat$sat[jobsat$country=="ME"],
-  main="", xlab="Job satisfaction (Mexico)")
+hist(df$sat[df$country=='CH'], main='', xlab='Job satisfaction (Chile)')
+hist(df$sat[df$country=='ME'], main='', xlab='Job satisfaction (Mexico)')
 
 # Wilcoxon rank sum test #
-wilcox.test(jobsat$sat[jobsat$country=="CH"], jobsat$sat[jobsat$country=="ME"],
-  paired=F)
+wilcox.test(df$sat[df$country=='CH'], df$sat[df$country=='ME'], paired=FALSE)
 
 # One-way ANOVA test #
-aov.model <- aov(formula=fm, data=jobsat)
+aov.model = aov(formula=fm, data=df)
 summary(aov.model)
 
 # Residual analysis #
-hist(aov.model$residuals, main="", xlab="ANOVA residuals")
-qqnorm(aov.model$residuals, main="", xlab="Standard normal quantiles",
-  ylab="ANOVA residuals", pch=20)
+hist(aov.model$residuals, main='', xlab='ANOVA residuals')
+qqnorm(aov.model$residuals, main='', xlab='Standard normal quantiles',
+  ylab='ANOVA residuals', pch=20)
 qqline(aov.model$residuals)
 
 # Kruskal-Wallis test #
-kruskal.test(formula=fm, data=jobsat)
+kruskal.test(formula=fm, data=df)
 
 # Linear regression (2 groups) #
-reg.model1 <- lm(formula=fm, data=df1)
+reg.model1 = lm(formula=fm, data=df[df$country!='SP', ])
 summary(reg.model1)
 
 # Linear regression (3 groups) #
-reg.model2 <- lm(formula=fm, data=df)
+reg.model2 = lm(formula=fm, data=df)
 summary(reg.model2)
