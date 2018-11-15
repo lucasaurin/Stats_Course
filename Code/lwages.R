e8b3d3e041ee3eb7133e01255ca6fb9f9535fb4e
mcanela-iese <38321496+mcanela-iese@users.noreply.github.com>
2018-11-15 16:36:47 +0100

    Update lwages.R

--- Code/lwages.R
+++ Code/lwages.R
@@ -1,34 +1,37 @@
 ## lwages data set ##
 
-# Importing data (customize) #
-lwage <- read.csv("lwages.csv")
-str(lwage)
-N <- nrow(lwage)
+# Importing data #
+df = read.csv('https://raw.githubusercontent.com/mcanela-iese/Stats_Course/master/Data/lwages.csv')
+str(df)
+N = nrow(df)
 
 # Salary change #
-change <- lwage$lwage7 - lwage$lwage0
+change = df$lwage7 - df$lwage0
 
 # Histogram #
-hist(change, main="", xlab="Log wages")
+hist(change, main='', xlab='Log wages')
 
 # Confidence limits #
-lcl <- mean(change) - qt(0.975, df=N-1)*sd(change)/sqrt(N)
-ucl <- mean(change) + qt(0.975, df=N-1)*sd(change)/sqrt(N)
+lcl = mean(change) - qt(0.975, df=N-1)*sd(change)/sqrt(N)
+ucl = mean(change) + qt(0.975, df=N-1)*sd(change)/sqrt(N)
 
 # t statistic #
-t <- mean(change)/(sd(change)/sqrt(N))
-t_alpha <- qt(0.975, df=N-1)
+t = mean(change)/(sd(change)/sqrt(N))
+t
+t_alpha = qt(0.975, df=N-1)
+t_alpha
 
 # p-value #
-p <- 2*(1 - pt(t, df=N-1))
+p = 2*(1 - pt(t, df=N-1))
+p
 
 # Function t.test #
 t.test(change)
-t.test(lwage$lwage7, lwage$lwage0, paired=T)
+t.test(lwage$lwage7, lwage$lwage0, paired=TRUE)
 
 # Correlation #
-plot(lwage7 ~ lwage0, data=lwage, xlab="Log wages (1980)",
-  ylab="Log wages (1987)", pch=20)
+plot(lwage7 ~ lwage0, data=lwage, xlab='Log wages (1980)',
+  ylab='Log wages (1987)', pch=20)
 
 # Wilcoxon signed rank test #
-wilcox.test(lwage$lwage7, lwage$lwage0, paired=T)
+wilcox.test(df$lwage7, df$lwage0, paired=TRUE)
