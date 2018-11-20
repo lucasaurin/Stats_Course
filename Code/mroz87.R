bc784d5fcab30710e4077ac24b64990c1ccf13f0
mcanela-iese <38321496+mcanela-iese@users.noreply.github.com>
2018-11-20 11:37:17 +0100

    Update mroz87.R

--- Code/mroz87.R
+++ Code/mroz87.R
@@ -1,15 +1,15 @@
 ## mroz87 data set ##
 
-# Importing data (customize) #
-mroz <- read.csv("mroz87.csv")
-str(mroz)
+# Importing data #
+df = read.csv('https://raw.githubusercontent.com/mcanela-iese/Stats_Course/master/Data/mroz87.csv')
+str(df)
 
 # Regression line #
-fm1 <- wage ~ we 
-mod1 <- lm(formula=fm1, data=mroz)
+fm1 = wage ~ we 
+mod1 = lm(formula=fm1, data=df)
 summary(mod1)
 
 # Multiple regression #
-fm2 <- wage ~ . 
-mod2 <- lm(formula=fm2, data=mroz)
+fm2 = wage ~ . 
+mod2 = lm(formula=fm2, data=df)
 summary(mod2)
