326f403230da50939dffbeb958c8bc91fdc40292
mcanela-iese <38321496+mcanela-iese@users.noreply.github.com>
2018-11-23 08:32:51 +0100

    Update wage1.R

--- Code/wage1.R
+++ Code/wage1.R
@@ -1,28 +1,28 @@
 ## wage1 data set ##
 
-# Importing data (customize) #
-df <- read.csv("wage1.csv")
+# Importing data #
+df = read.csv('https://raw.githubusercontent.com/mcanela-iese/Stats_Course/master/Data/wage1.csv')
 str(df)
 
 # Unrestricted model #
-ur <- log(wage) ~ .
-ur.model <- lm(formula=ur, data=df)
+ur = log(wage) ~ .
+ur.model = lm(formula=ur, data=df)
 summary(ur.model)
 
 # Restricted model (1) #
-re1 <- log(wage) ~ educ + tenure
-re1.model <- lm(formula=re1, data=df)
+re1 = log(wage) ~ educ + tenure
+re1.model = lm(formula=re1, data=df)
 summary(re1.model)
 anova(ur.model, re1.model)
 
 # Restricted model (2) #
-re2 <- log(wage) ~ 1
-re2.model <- lm(formula=re2, data=df)
+re2 = log(wage) ~ 1
+re2.model = lm(formula=re2, data=df)
 summary(re2.model)
 anova(ur.model, re2.model)
 
 # Restricted model (3) #
-re3 <- log(wage) ~ educ
-re3.model <- lm(formula=re3, data=df)
+re3 = log(wage) ~ educ
+re3.model = lm(formula=re3, data=df)
 summary(re3.model)
 anova(ur.model, re3.model)
