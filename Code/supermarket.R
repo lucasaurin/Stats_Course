8be6296fa4f22eff354c4cf26037cb60caf99c44
mcanela-iese <38321496+mcanela-iese@users.noreply.github.com>
2018-11-26 09:16:15 +0100

    Add files via upload

--- Code/supermarket.R
+++ Code/supermarket.R
@@ -1,27 +1,27 @@
 ## supermarket data set ##
 
-# Reading data (customize) #
-super <- read.csv("supermarket.csv")
-summary(super)
+# Importing data #
+df = read.csv('https://raw.githubusercontent.com/mcanela-iese/Stats_Course/master/Data/supermarket.csv')
+str(df)
 
 # Correlation analysis #
-cor(super[,-1])
+cor(df[,-1])
 
 # Regression analysis #
-fm1 <- reuse ~ type + perfo + conv + employee + sat
-mod1 <- lm(formula=fm1, data=super)
+fm1 = reuse ~ .
+mod1 = lm(formula=fm1, data=df)
 summary(mod1)
 
 # Standardized coefficients #
 std = function(x) (x-mean(x))/sd(x)
-fm2 <- std(reuse) ~ type + std(perfo) + std(conv) + std(employee) +
+fm2 = std(reuse) ~ type + std(perfo) + std(conv) + std(employee) +
   std(sat)
-mod2 <- lm(formula=fm2, data=super)
+mod2 = lm(formula=fm2, data=df)
 summary(mod2)
 
 # Interaction terms #
-fm3 <- std(reuse) ~ type + std(perfo) + I(type*std(perfo)) +
+fm3 = std(reuse) ~ type + std(perfo) + I(type*std(perfo)) +
   std(conv) + I(type*std(conv)) + std(employee) +
   I(type*std(employee)) + std(sat) + I(type*std(sat))
-mod3 <- lm(formula=fm3, data=super)
+mod3 = lm(formula=fm3, data=df)
 summary(mod3)
